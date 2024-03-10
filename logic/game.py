36f2107a3438a80a69d9785b9502cac68b82daf1
Patrutcht <patrice.andres.lycee@gmail.com>
2024-03-10 11:32:17 +0100

    Game - init player in game

--- logic/game.py
+++ logic/game.py
@@ -1,7 +1,13 @@
+from pygame import Color
 from board import Board
+from player import Player
 
 WIDTH: int = 500
 HEIGHT: int = 500
+RED = Color(255, 0, 0)
+GREEN = Color(0, 255, 0)
+BLUE = Color(0, 0, 255)
+COLORS = [RED, BLUE, GREEN]
 
 
 class Game:
@@ -17,8 +23,12 @@ class Game:
         Constructor of Game
         :param nb_player: number of player
         :param target_score: score to reach
+        :param width: width of the board
+        :param height: height of the board
         """
-
         self.players = []
+        for n in range(nb_player):
+            player_name = input(f'Player {n + 1} name: ')
+            self.players.append(Player(player_name, COLORS[n]))
         self.target_score = target_score
         self.board = Board(width, height)
